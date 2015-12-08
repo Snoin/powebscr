54821110c02a3d4edc3a5e0508e8a6594518d0d8
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2015-12-08 13:28:56 +0900

    powebscr.legacy: move lagacy thing from snoin.com repo

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 cd "$( dirname ${BASH_SOURCE[0]})"
 
-echo "install pre-commit hook (forced)"
-ln -sf ../../hooks/pre-commit .git/hooks/pre-commit
-echo
-
-echo "setup basic environ for develop in python"
-pip install -e .[tests]
-echo
-
-echo "test the pre-commit hook"
-exec .git/hooks/pre-commit
+./powebscor/legacy/install_libicu52.sh
+pip install -e .[legacy]
---
+++ setup-legacy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$( dirname ${BASH_SOURCE[0]})"
+
+echo "install pre-commit hook (forced)"
+ln -sf ../../hooks/pre-commit .git/hooks/pre-commit
+echo
+
+echo "setup basic environ for develop in python"
+pip install -e .[tests]
+pip install -e .[legacy]
+echo
+
+echo "test the pre-commit hook"
+exec .git/hooks/pre-commit
